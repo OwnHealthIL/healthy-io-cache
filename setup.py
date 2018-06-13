11df3284d49a748847b80b8152ae9083b5083023
noamt <noam@healthy.io>
2018-06-13 12:45:54 +0300

    Bumping release version and redis client version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='HealthyIoCache',
-    version='0.0.1',
+    version='0.0.2',
     packages=['healthyiocache.redis', ],
     long_description='The Backend\'s wrapper for all cache operations',
     url='https://github.com/OwnHealthIL/healthy-io-cache',
@@ -12,5 +12,5 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable'
     ],
-    install_requires=['redis==2.10.3'],
+    install_requires=['redis==2.10.6'],
 )
